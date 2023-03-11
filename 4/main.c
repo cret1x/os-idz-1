d24d0937831884d51d5d41e823620f2c4f9fb2bb
cret1x <cretixthebest@gmail.com>
2023-03-11 16:20:48 +0300

    init

---
+++ 4/main.c
@@ -0,0 +1,56 @@
+// 02-parent-child.c
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const int buf_size = 25;
+// const int buf_size = 10;
+const int mes_size = 14;
+// const int mes_size = 5;
+
+int main() {
+  int    fd[2], result;
+  size_t size;
+  char   str_buf[buf_size];
+
+  if(pipe(fd) < 0) {
+    printf("Can\'t open pipe\n");
+    exit(-1);
+  }
+
+  result = fork();
+  if(result < 0) {
+    printf("Can\'t fork child\n");
+    exit(-1);
+  } else if (result > 0) { /* Parent process */
+    if(close(fd[0]) < 0){
+        printf("parent: Can\'t close reading side of pipe\n"); exit(-1);
+    }
+    size = write(fd[1], "Hello, world!", mes_size);
+    if(size != mes_size){
+      printf("Can\'t write all string to pipe\n");
+      exit(-1);
+    }
+    if(close(fd[1]) < 0) {
+      printf("parent: Can\'t close writing side of pipe\n");
+      exit(-1);
+    }
+    printf("Parent exit\n");
+  } else { /* Child process */
+    if(close(fd[1]) < 0){
+      printf("child: Can\'t close writing side of pipe\n"); exit(-1);
+    }
+    size = read(fd[0], str_buf, mes_size);
+    if(size < 0){
+        printf("Can\'t read string from pipe\n");
+        exit(-1);
+    }
+    printf("Child exit, str_buf: %s\n", str_buf);
+    if(close(fd[0]) < 0){
+        printf("child: Can\'t close reading side of pipe\n"); exit(-1);
+    }
+  }
+
+  return 0;
+}

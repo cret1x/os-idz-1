aef58fa6a206dff3fdd8c70e81313a8e314c9355
cret1x <cretixthebest@gmail.com>
2023-03-19 23:09:46 +0300

    finish

--- 10/proc1.c
+++ 10/proc1.c
@@ -9,12 +9,15 @@
 #include <string.h>
 
 
-#define queue_key 451
+#define send_key 451
+#define recv_key 452
 #define MSGSZ 128
+#define RMSGSZ MSGSZ + sizeof(ssize_t)
 
 typedef struct msgbuf
 {
     long mtype;
+    ssize_t msize;
     char mtext[MSGSZ];
 } msgbuf;
 
@@ -23,19 +26,72 @@ int main(int argc, char ** argv) {
         printf("Usage: proc1 <input> <output>\n");
         exit(0);
     }
-    int msgid;
-    size_t buf_len;
-    msgbuf sbuf;
-    msgid = msgget(queue_key, IPC_CREAT | 0666);
-    printf("Queue created id = %d\n", msgid);
-    sbuf.mtype = 1;
-    strcpy(sbuf.mtext, "Fuck that shit");
-    buf_len = strlen(sbuf.mtext) + 1;
-
-    if (msgsnd(msgid, &sbuf, buf_len, IPC_NOWAIT) < 0) {
-        printf("Error\n");
+    int fin;
+    int fout;
+    int send_qid;
+    int recv_qid;
+    size_t buffer_len;
+    ssize_t read_bytes;
+    ssize_t written_bytes;
+    struct msqid_ds qinfo;
+    msgbuf send_buffer;
+    msgbuf recv_buffer;
+    char buffer[MSGSZ];
+    // create queue
+    if((send_qid = msgget(send_key, IPC_CREAT | 0666)) < 0) {
+        printf("Error creating seding queue!\n");
+        exit(-1);
+    } else {
+        printf("Queue created id = %d\n", send_qid);
     }
-    printf("Sent! - %s\n",sbuf.mtext);
+    if((recv_qid = msgget(recv_key, IPC_CREAT | 0666)) < 0) {
+        printf("Error creating recieving queue!\n");
+        exit(-1);
+    } else {
+        printf("Queue created id = %d\n", recv_qid);
+    }
 
+    if ((fin = open(argv[1], O_RDONLY)) < 0) {
+        printf("Can\'t open input file\n");
+        exit(-1);
+    }
+    if ((fout = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IXUSR)) < 0) {
+        printf("Can\'t create output file\n");
+        exit(-11);
+    }
+
+    while ((read_bytes = read(fin, buffer, MSGSZ)) > 0) {
+        send_buffer.mtype = 1;
+        send_buffer.msize = read_bytes;
+        memcpy(send_buffer.mtext, buffer, read_bytes);
+        if (msgsnd(send_qid, &send_buffer, RMSGSZ, IPC_NOWAIT) < 0) {
+            printf("Error sending message!\n");
+            close(fin);
+            close(fout);
+            exit(-1);
+        }
+        printf("Sent message of %ld bytes\n",read_bytes);
+    }
+    close(fin);
+    int qmsgsc = 0;
+    printf("Waiting for second process...\n");
+    do {
+        if (msgrcv(recv_qid, &recv_buffer, RMSGSZ, 2, 0) < 0) {
+            printf("Error recieving message!\n");
+            close(fin);
+            close(fout);
+            exit(-1);
+        }
+        written_bytes = write(fout, recv_buffer.mtext, recv_buffer.msize);
+        printf("Recv message of %ld bytes\n",written_bytes);
+        // get number of messages in queue
+        if ((msgctl(recv_qid, IPC_STAT, &qinfo)) < 0) {
+            printf("Error getting info about recv queue!\n");
+            exit(-1);
+        }
+        qmsgsc = (int) qinfo.msg_qnum;
+        printf("Messages left: %d\n", qmsgsc);
+    } while (qmsgsc > 0);
+    close(fout);
     return 0;
 }
--- 10/proc2.c
+++ 10/proc2.c
@@ -8,26 +8,75 @@
 #include <fcntl.h>
 #include <string.h>
 
-
-#define queue_key 451
+// swapped keys!!!!!!
+#define recv_key 451
+#define send_key 452
 #define MSGSZ 128
+#define RMSGSZ MSGSZ + sizeof(ssize_t)
 
 typedef struct msgbuf
 {
     long mtype;
+    ssize_t msize;
     char mtext[MSGSZ];
 } msgbuf;
 
 int main(int argc, char ** argv) {
-    
-    int msgid;
-    msgbuf sbuf;
-    msgid = msgget(queue_key, 0666);
-    printf("Queue opened id = %d\n", msgid);
-    if (msgrcv(msgid, &sbuf, MSGSZ, 1, 0) < 0) {
-        printf("Error\n");
+    int send_qid;
+    int recv_qid;
+    int qmsgsc = 1;
+    size_t buffer_len;
+    ssize_t read_bytes;
+    ssize_t written_bytes;
+    msgbuf send_buffer;
+    msgbuf recv_buffer;
+    struct msqid_ds qinfo;
+    char buffer[MSGSZ];
+    // create queue
+    if((send_qid = msgget(send_key, IPC_CREAT | 0666)) < 0) {
+        printf("Error creating seding queue!\n");
+        exit(-1);
+    } else {
+        printf("Queue created id = %d\n", send_qid);
     }
-    printf("RECV! - %s\n",sbuf.mtext);
+    if((recv_qid = msgget(recv_key, IPC_CREAT | 0666)) < 0) {
+        printf("Error creating recieving queue!\n");
+        exit(-1);
+    } else {
+        printf("Queue created id = %d\n", recv_qid);
+    }
 
+    printf("Waiting for first process...\n");
+    do {
+        if (msgrcv(recv_qid, &recv_buffer, RMSGSZ, 1, 0) < 0) {
+            printf("Error recieving message!\n");
+            exit(-1);
+        }
+        printf("Recv message of %ld bytes\n", recv_buffer.msize);
+        memcpy(buffer, recv_buffer.mtext, recv_buffer.msize);
+        for (int i = 0; i < recv_buffer.msize; i++) {
+            if ((buffer[i] >= 'A' && buffer[i] <= 'Z') || (buffer[i] >= 'a' && buffer[i] <= 'z')) {
+                if (buffer[i] != 'e' && buffer[i] != 'y' && buffer[i] != 'u' && buffer[i] != 'i' && buffer[i] != 'o' && buffer[i] != 'a') {
+                    if (buffer[i] >= 'a') {
+                        buffer[i] -= 32;
+                    }
+                } 
+            }
+        }
+        send_buffer.mtype = 2;
+        send_buffer.msize = recv_buffer.msize;
+        memcpy(send_buffer.mtext, buffer, recv_buffer.msize);
+        if (msgsnd(send_qid, &send_buffer, RMSGSZ, IPC_NOWAIT) < 0) {
+            printf("Error sending message!\n");
+            exit(-1);
+        }
+        // get number of messages in queue
+        if ((msgctl(recv_qid, IPC_STAT, &qinfo)) < 0) {
+            printf("Error getting info about recv queue!\n");
+            exit(-1);
+        }
+        qmsgsc = (int) qinfo.msg_qnum;
+        printf("Messages left: %d\n", qmsgsc);
+    } while (qmsgsc > 0);
     return 0;
 }
